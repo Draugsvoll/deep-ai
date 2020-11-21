acf6a1cae5930f8158fe71d1c6f2c31374246f30
Draugsvoll <ove.hd91@gmail.com>
2020-11-21 19:15:22 +0100

    upd

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,10 @@
     </div>
     <!-- SELECTED IMAGE -->
     <div class="selected-img-container">
-      <div>here</div>
+      <div>Selected Image:</div>
       <img class="selected-img" v-bind:src="selectedImage" alt="" srcset="">
+      <div>Get caption</div>
+      <button @click="getCaption">Caption</button>
     </div>
   </div>
 </template>
@@ -35,6 +37,7 @@ export default {
 
   },
   methods: {
+    // search images
     search (searchTerm) {
       var images = []
       console.log('Search term: ', searchTerm)
@@ -45,24 +48,42 @@ export default {
       headers: {
         'x-rapidapi-key': '624dc7754bmsh3f19b0e1fbd4882p18e7f1jsn0d9d641d8df8',
         'x-rapidapi-host': 'bing-image-search1.p.rapidapi.com'
-      }
-    };
-    axios.request(options).then(function (response) {
-      response = response.data.value
-      Object.entries(response).forEach( image => {
-        const newImage = image[1].contentUrl
-        images.push(newImage)
-      })
-    }).catch(function (error) {
-      console.error(error);
-    });
-    this.images = images
-    },// search
-    //* select image
+      }};
+      axios.request(options).then(function (response) {
+        response = response.data.value
+        Object.entries(response).forEach( image => {
+          const newImage = image[1].contentUrl
+          images.push(newImage)
+        })
+      }).catch(function (error) {
+        console.error(error);
+      });
+      this.images = images
+    },
+    // select an image
     selectImage(image) {
       this.selectedImage = image
       console.log('image select: ', image)
       
+    },
+    // get Caption
+    getCaption () {
+      const data = `image=${this.selectedImage}`;
+
+      const xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+          console.log(this.responseText);
+        }
+      });
+      xhr.open("POST", "https://deepai-deepai-computer-vision-v1.p.rapidapi.com/api/neuraltalk");
+      xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+      xhr.setRequestHeader("api-key", "a570693f-0601-4674-9737-0c617e3668d6");
+      xhr.setRequestHeader("x-rapidapi-key", "624dc7754bmsh3f19b0e1fbd4882p18e7f1jsn0d9d641d8df8");
+      xhr.setRequestHeader("x-rapidapi-host", "deepai-deepai-computer-vision-v1.p.rapidapi.com");
+      xhr.send(data);
     }
   }//methods
 
